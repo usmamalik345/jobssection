57fb181723ff520102b377a3a6e9c932c40e2b10
usmamalik345 <usmamalik344@gmail.com>
2022-08-05 05:45:51 -0700

    done with the job portal desgn

---
+++ src/Dropdown.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Dropdown = () => {
+    const [showoption, setshowoption] = useState(false)
+    const handleclick = () => {
+        setshowoption(!showoption)
+    }
+  return (
+    <div>
+        <div class="relative inline-block text-left ">
+  <div>
+    <button onClick={ handleclick} type="button" className=" inline-flex justify-center w-full rounded-md    px-4 py-2 bg-white text-sm font-medium text-gray-700     can-edit" id="menu-button" aria-expanded="true" aria-haspopup="true">
+      Can edit 
+     
+      <svg className="-mr-1 ml-2 h-5 w-5 svg_color" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+
+
+  { showoption && (<div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div className="py-1  dropdown_menu" role="none">
+      
+      
+      
+    </div>
+  </div>)}
+</div>
+    </div>
+  )
+}
+
+export default Dropdown
--- src/Jobportal.js
+++ src/Jobportal.js
@@ -10,14 +10,35 @@ import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import PersonIcon from "@mui/icons-material/Person";
+import CircleIcon from '@mui/icons-material/Circle';
+import FlagIcon from '@mui/icons-material/Flag';
+import Flag from "@mui/icons-material/Flag";
+// Drop down menu itemsss
+// import Dropdown from "./Dropdown";
 
-const Jobportal = () => {
+const Jobportal = (props) => {
   return (
     <div>
-      <Container sx={{ display: "flex" }}>
-        <Box>
-          <Card sx={{ width: "552px", height: "351px", mr: "30px"  }}>
+      <Container
+        sx={{
+          display: "flex",
+          borderRadius: "20px",
+          boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+        }}
+      >
+        <Box sx={{ mt: "30px" }}>
+          <Card
+            sx={{
+              width: "552px",
+              height: "351px",
+              mr: "30px",
+              border: 1,
+              borderRadius: "10px",
+            }}
+          >
             <CardContent>
               <Typography
                 sx={{ fontSize: "20px" }}
@@ -48,8 +69,6 @@ const Jobportal = () => {
                 RawalPindi
               </Typography>
 
-            
-
               <Typography
                 sx={{
                   mb: "12px",
@@ -69,13 +88,12 @@ const Jobportal = () => {
               </Typography>
               <Typography
                 variant="body2"
-                sx={{ disply: "flex", flexDirection: "row"  }}
+                sx={{ disply: "flex", flexDirection: "row" }}
               >
                 <SendIcon sx={{ fontSize: "14px", mr: "7px", color: "blue" }} />{" "}
                 Apply From your Phone
                 <PersonAddAlt1Icon
                   sx={{
-                    
                     ml: "18px",
                     color: "blue",
                   }}
@@ -84,59 +102,244 @@ const Jobportal = () => {
               </Typography>
             </CardContent>
             <CardActions>
-                <Box  >
-                <Typography >
-                <RadioButtonUncheckedIcon  sx={{fontSize:"10px ", mr: "10px" }}/> 
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestiae.
-                  </Typography>
+              <Box>
+                <Typography
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                  }}
+                >
+                  <RadioButtonUncheckedIcon
+                    sx={{ fontSize: "10px ", mr: "10px" }}
+                  />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur, molestiae.
+                </Typography>
 
-                  <Typography >
-                <RadioButtonUncheckedIcon  sx={{fontSize:"10px ", mr: "10px" }}/> 
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestiae.
+                <Typography
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                  }}
+                >
+                  <RadioButtonUncheckedIcon
+                    sx={{ fontSize: "10px ", mr: "10px" }}
+                  />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequuntur, molestiae.
+                </Typography>
+
+                <Typography
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                    ml: "10px",
+                    mt: "10px",
+                  }}
+                >
+                  16 days ago. Based on
+                  <Typography sx={{ ml: "10px" }}>
+                    <b> jobs you explored</b>
                   </Typography>
-                  <Typography >
-                <RadioButtonUncheckedIcon  sx={{fontSize:"10px ", mr: "10px" }}/> 
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestiae.
-                  </Typography>
-                  <Typography >
-                <RadioButtonUncheckedIcon  sx={{fontSize:"10px ", mr: "10px" }}/> 
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, molestiae.
-                  </Typography>
-                  </Box> 
-                
+                </Typography>
+              </Box>
             </CardActions>
           </Card>
-
-                    
-
-
         </Box>
 
-        <Box>
-          <Card sx={{ width: "430px", height: "692px" }}>
+        <Box sx={{ mt: "30px", mb: "30px" }}>
+          <Card
+            sx={{
+              width: "500px",
+              height: "620px",
+              border: 1,
+              borderRadius: "10px",
+            }}
+          >
             <CardContent>
               <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
+                sx={{ fontSize: "20px" }}
+                color="black"
                 gutterBottom
+                fontWeight="700"
               >
                 Online Working
               </Typography>
-              <Typography variant="h5" component="div">
-                be
+              <Typography
+                variant="h7"
+                component="div"
+                fontWeight="400"
+                fontSize="16px"
+                marginTop="10px"
+                color="blue"
+              >
+                EFU LIFE ASSURANCE
               </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
+              <Typography
+                variant="h7"
+                component="div"
+                fontWeight="300"
+                fontSize="18px"
+                marginButtom="80px"
+              >
+                RawalPindi
               </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
+              <Typography variant="body2" sx={{ display: "flex" }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    mt: "20px",
+                    color: "back",
+                    fontWeight: "600",
+                    borderRadius: "10px",
+                  }}
+                >
+                  Apply Now
+                </Button>
+                <Typography>
+                  <FavoriteIcon
+                    sx={{ fontSize: "40px", mt: "20px", ml: "30px" }}
+                  />
+                </Typography>
               </Typography>
+              <Typography sx={{ borderBottom: 1, mt: "20px" }}></Typography>
             </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
+            <Box sx={{ width: "484px", height: "413px", overflow: "auto" }}>
+              <Typography
+                sx={{
+                  fontSize: "20px",
+                  fontWeight: "600",
+                  ml: "12px",
+                  mb: "30px",
+                }}
+              >
+                Jobs details
+              </Typography>
+
+              <Typography
+                sx={{ fontSize: "15px", fontWeight: "300", ml: "12px" }}
+              >
+                Job-Type
+              </Typography>
+
+              <Typography
+                sx={{ fontSize: "15px", fontWeight: "300", ml: "12px" }}
+              >
+                Full-Time
+              </Typography>
+
+              <Typography
+                sx={{ fontSize: "15px", fontWeight: "300", ml: "12px" }}
+              >
+                Part-Time
+              </Typography>
+
+              <Typography sx={{ borderBottom: 1, mt: "20px" }}></Typography>
+
+              <Typography
+                sx={{
+                  fontSize: "23px",
+                  fontWeight: "600",
+                  ml: "12px",
+                  mt: "15px",
+                }}
+              >
+                Full Job Description
+              </Typography>
+
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  ml: "12px",
+                  mb: "10px",
+                }}
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Cupiditate velit illo autem iste maxime? Veniam quibusdam nemo
+                doloribus deleniti natus?
+              </Typography>
+
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  ml: "12px",
+                  mb: "10px",
+                }}
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                laborum iste reiciendis. Quidem voluptatum minus fuga fugit,
+                fugiat ea alias.
+              </Typography>
+
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  ml: "12px",
+                  mb: "10px",
+                }}
+              >
+                Job Type: Full-time,Part-time
+              </Typography>
+
+              <Typography sx={{ borderBottom: 1, mt: "20px" }}></Typography>
+
+              <Typography
+                sx={{
+                  fontSize: "23px",
+                  fontWeight: "600",
+                  ml: "12px",
+                  mt: "15px",
+                }}
+              >
+                Hiring Insights
+              </Typography>
+
+              <Box sx={{ display: "flex", flexDirection: "row" , ml: "10px" }}>
+                <PersonIcon sx={{ fontSize: "17px" }} />
+                <Typography sx={{ ml: "10px" }}>
+                  Hiring <b> 2</b> candidates for this role
+                </Typography>
+              </Box>
+
+              <Typography
+                sx={{
+                  fontSize: "15px",
+                  fontWeight: "600",
+                  ml: "10px",
+                  mt: "15px",
+                }}
+              >
+                Job activity
+              </Typography>
+
+              <Typography sx={{ display: "flex", flexDirection: "row", ml: "10px" }}>
+                        <CircleIcon  sx={{fontSize: "10px" , mt: "6px" , mr: "10px"}} /> 
+
+                        <Typography>
+                        
+                        Employer reviewed job 20 days ago
+
+                        </Typography>
+              </Typography>
+
+              <Typography sx={{ borderBottom: 1, mt: "20px" }}></Typography>
+                
+                <Typography sx={{fontSize: "13px" , ml: "10px"}}>
+                        17 days a ago 
+
+
+                    </Typography> 
+
+                   <Button variant="contained" sx={{ fontWeight: "600" , ml: "10px" , mt: "20px"}}> <Flag sx={{color: "black" }}/>  Report Job</Button>
+    
+
+            </Box>
           </Card>
         </Box>
       </Container>
